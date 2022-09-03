d30067fbc6a0d7409fc92754353ead17f8180beb
arturs.langenfelds@28stone.com <alangenfeld@gmail.com>
2022-09-03 20:32:06 +0300

    Posts board with reaction buttons

---
+++ 02_lesson_starter/src/features/posts/PostsList.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
+
+const PostsList = () => {
+  //   const posts = useSelector((state) => state.posts);
+  const posts = useSelector(selectAllPosts);
+
+  // slice() will return a shallow copy of array
+  const orderedPosts = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderPosts = orderedPosts.map((post) => (
+    <article key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content.substring(0, 100)}</p>
+      <PostAuthor userId={post.userId} />
+      <TimeAgo timestamp={post.date} />
+      <ReactionButtons post={post}/>
+    </article>
+  ));
+
+  return (
+    <section>
+      <h2>Posts</h2>
+      {renderPosts}
+    </section>
+  );
+};
+
+export default PostsList;
